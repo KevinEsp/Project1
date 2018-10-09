567fe0edf28cd237c729ca4fc8c87a747d5dee59
KevinEsp <Kevin.Espenshade@publicans.com>
2018-10-09 16:47:33 -0500

    fixed again

--- src/mypack/MySecondClass.java
+++ src/mypack/MySecondClass.java
@@ -1,10 +1,6 @@
 package mypack;
 
-<<<<<<< HEAD
 public class MySecondClass {
-=======
-public class MyClass {
->>>>>>> refs/remotes/origin/master
 
     public static void main(String[] args) {
         System.out.println("Running project 1...");
